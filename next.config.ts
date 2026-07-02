5567d36c2c94c2e732db48d2626148181e9bf066
ramdotcode <ramdotcode@gmail.com>
2026-07-02 08:35:16 +0700

    chore: redirect root path (/) to (/kia-ara) in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/kia-ara",
+        permanent: true, // 301 Redirect (Permanent)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
